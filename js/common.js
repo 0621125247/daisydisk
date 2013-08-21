fda445fcd73180ca07e90fdb1068c2e4aaa7ee8a
Yevgen <godun.yevgen@gmail.com>
2013-08-21 17:36:53 +0300

    begin SLIDER

--- js/common.js
+++ js/common.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
+    // $(function() {
+    //   // initialize scrollable
+    //   $(".scrollable").scrollable();
+    // });
 
+    // $(".calendar-slider__item:first").next().next().children().addClass("calendar_active");
+    // $(".day-next, .day-prev").click(function(){
+    //     $(".calendar").removeClass("calendar_active");
+    // });
+    $('.scrollable').scrollable({
+        circular: true,
+        onSeek: function() {
+            var index = this.getItems().eq(this.getIndex());
+            $(index).next().addClass("is-active");
+        }
+    });
+    $(".browse").click(function(){
+        $(".items img").removeClass("is-active");
+    });
+
+    //api = $('.scrollable').data('scrollable');
 });

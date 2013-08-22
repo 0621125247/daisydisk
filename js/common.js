4b09c89df4497a9174c71520d7c91f09fa449e61
Yevgen <godun.yevgen@gmail.com>
2013-08-22 13:13:11 +0300

    upd slider

--- js/common.js
+++ js/common.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
     var active_slide = $(".cycle-slide-active");
     active_slide.next().addClass("is-big");
-    $( '.js-slider' ).on( 'cycle-next', function( event, opts ) {
+    // $( '.js-slider' ).on( 'cycle-next', function( event, opts ) {
+    //     $(".cycle-slide").removeClass("is-big");
+    //     $(".cycle-slide-active").next().next().addClass("is-big");
+    // });
+    // $( '.js-slider' ).on( 'cycle-prev', function( event, opts ) {
+    //     $(".cycle-slide").removeClass("is-big");
+    //     $(".cycle-slide-active").addClass("is-big");
+    // });
+    // $( '.js-slider' ).on( 'cycle-prev', function( event, opts ) {
+    //     $(".cycle-slide").removeClass("is-big");
+    //     $(".cycle-slide-active").addClass("is-big");
+    // });
+    $( '.js-slider' ).on( 'cycle-before', function( event, opts ) {
         $(".cycle-slide").removeClass("is-big");
         $(".cycle-slide-active").next().next().addClass("is-big");
     });
-    $( '.js-slider' ).on( 'cycle-prev', function( event, opts ) {
+    $( '.js-slider' ).on( 'cycle-after', function( event, opts ) {
         $(".cycle-slide").removeClass("is-big");
-        $(".cycle-slide-active").addClass("is-big");
+        $(".cycle-slide-active").next().next().addClass("is-big");
+    });
+    $( '.js-slider' ).on( 'cycle-transition-stopped', function( event, opts ) {
+        // $(".cycle-slide").removeClass("is-big");
+        // $(".cycle-slide-active").next().next().addClass("is-big");
+        alert();
     });
 });

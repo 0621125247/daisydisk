26091460d9543ff4ded5149f0e851f2873d7ae50
Yevgen <godun.yevgen@gmail.com>
2013-08-22 14:58:26 +0300

    pixel perfect

--- js/common.js
+++ js/common.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
         $(".cycle-slide").removeClass("is-big");
         $(".cycle-slide-active").next().next().addClass("is-big");
     });
-    $( '.js-slider' ).on( 'cycle-transition-stopped', function( event, opts ) {
+    $( '.js-slider' ).on( 'cycle-pager-activated', function( event, opts ) {
         // $(".cycle-slide").removeClass("is-big");
         // $(".cycle-slide-active").next().next().addClass("is-big");
         alert();

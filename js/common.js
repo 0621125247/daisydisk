42721570f5174c3cea27007f5783b77af93a0e7f
Yevgen <godun.yevgen@gmail.com>
2013-08-22 10:56:25 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,23 +1,12 @@
 $(document).ready(function() {
-    // $(function() {
-    //   // initialize scrollable
-    //   $(".scrollable").scrollable();
-    // });
-
-    // $(".calendar-slider__item:first").next().next().children().addClass("calendar_active");
-    // $(".day-next, .day-prev").click(function(){
-    //     $(".calendar").removeClass("calendar_active");
-    // });
-    $('.scrollable').scrollable({
-        circular: true,
-        onSeek: function() {
-            var index = this.getItems().eq(this.getIndex());
-            $(index).next().addClass("is-active");
-        }
+    var active_slide = $(".cycle-slide-active");
+    active_slide.next().addClass("is-big");
+    $( '.js-slider' ).on( 'cycle-next', function( event, opts ) {
+        $(".cycle-slide").removeClass("is-big");
+        $(".cycle-slide-active").next().next().addClass("is-big");
     });
-    $(".browse").click(function(){
-        $(".items img").removeClass("is-active");
+    $( '.js-slider' ).on( 'cycle-prev', function( event, opts ) {
+        $(".cycle-slide").removeClass("is-big");
+        $(".cycle-slide-active").addClass("is-big");
     });
-
-    //api = $('.scrollable').data('scrollable');
 });
